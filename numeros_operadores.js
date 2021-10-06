a59f0279d82d08fa34bbd2852c863738c128d91b
Fredson <85347233+fredsons@users.noreply.github.com>
2021-10-06 18:07:32 -0300

    adiciona njumeros e operadores

---
+++ numeros_operadores.js
@@ -0,0 +1,160 @@
+
+
+// NÚMEROS
+
+/*
+    var idade = 28;
+    var gols = 1000;
+    var pi = 3.14; // ponto para decimal
+    var exp = 2e10; // 20000000000
+
+    *Precisão para ate 15 digitos
+
+
+    // OPERADORES ARITIMÉTICOS
+
+    var soma = 100 + 50; // 150
+    var subtracao = 100 - 50; // 50
+    var multiplicacao = 100 * 2; // 200
+    var divisao 2 / 2; // 50
+    var expoente = 2 ** 4; // 16
+    var modulo 14 % 5; // 4
+
+    *Lembrando que soma + em string serve para concatenar
+
+
+    // OPERADORES ARITIMÉTICOS (STRINGS)
+
+    var soma = '100' + 50; // 10050
+    var subtracao = '100' - 50; // 50
+    var multiplicacao = '100' * '2'; // 200
+    var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
+    * É possivel verificar se uma variavel é NaN ou não com a função insNaN()
+
+    // NAN NOT A NUMBER
+
+    var numero = 80;
+    var unidade = 'Kg';
+    var peso  = numero + unidade; // '80Kg'
+    var pesoPorDois = peso / 2 // NaN (Not a Number)
+
+    // A ORDEM IMPORTA
+
+    * Começa por multiplicação e divisão, depois por soma e subtração
+
+    var total1 = 20 + 5 * 2; // 30
+    var total2 = (20 + 5) * 2; // 50
+    var total3 = 20 / 2 * 5; // 50
+    var total4 = 10 + 10  * 2 + 20 / 2; // 40
+
+    * Parenteses para priorizar uma expressão
+
+    // OPERADORES ARITIMÉTICOS UNÁRIOS
+
+    var incremento = 5;
+    console.log(incremento++); // 5
+    console.log(incremento); // 6
+
+    var incremento2 = 5;
+    console.log(++incremento2); // 6
+    console.log(incremento2); // 6
+
+    * Mesma coisa para decremento --
+
+    * O + e - tenta transformar o valor seguinte em número
+
+    var frase = 'isso é um teste';
+    +frase; // NaN
+    -frase; // NaN
+
+    var idade = '28';
+    +idade; // 28(numero)
+    -idade; // -28(numero)
+    console.log(+idade + 5); // 33
+    
+    
+    var possuiFaculdade = true;
+    console.log(+possuiFaculdade); // 1
+
+    * O - antes de um número torna ele negativo
+
+
+    // Qual o resultado da seguinte expressão?
+
+    var total = 10 + 5 * 2 / 2 + 20; >>>>> 35
+
+
+    // Crie duas expressões que retornem NaN
+
+    var expressao1 = 'Teste' / 2;
+    var expressao2 = 'Teste' - 2;
+    console.log(expressao1, expressao2);
+   
+    // Somar a string '200' com o número 50 e retornar 250
+     
+    var soma = +'200' + 50;
+    console.log(soma);
+
+
+    // Incremente o número 5 e retorne o seu valor incrementado
+
+    var incremento = 5;
+    console.log(incremento++);
+
+    // Como dividir o peso por 2?
+    var numero = +'80';
+    var unidade = 'kg';
+    var peso = numero  + unidade; // '80kg'
+    console.log(peso);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
