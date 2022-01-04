23850c6852af8d3211b9e95e5675ba762045459e
VerisureBrasil <57370866+VerisureBrasil@users.noreply.github.com>
2022-01-04 17:13:16 -0300

    altera comentarios

--- dom/dom.js
+++ dom/dom.js
@@ -1,73 +1,75 @@
 /*
 
-// DOM PARA INICIANTES
+    * DOM PARA INICIANTES
 
-* O que é o DOM
+    * O que é o DOM
 
-// DOCUMENT OBJECT MODEL (DOM)
+    * DOCUMENT OBJECT MODEL (DOM)
 
-* É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
+    * É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
 
-console.log(window);
-// window é o objeto global do browser
-// possui diferentes métodos e propriedades
+    console.log(window);
+    / window é o objeto global do browser
+    / possui diferentes métodos e propriedades
 
-window.innerHeight; // retorna a altura do browser
+    window.innerHeight; // retorna a altura do browser
 
-* Ao inspecionar elemento com o Chrome, você está vendo a representação oficial do DOM.
+    * Ao inspecionar elemento com o Chrome, você está vendo a representação oficial do DOM.
 
-// WINDOW E DOCUMENT
+    * WINDOW E DOCUMENT
 
-* São os objetos principais do DOM, boa parte da manipulação é feita através dos seus métodos e propriedades.
+    * São os objetos principais do DOM, boa parte da manipulação é feita através dos seus métodos e propriedades.
 
-window.alert('Isso é um alerta');
-alert('Isso é um alerta'); // Funciona
+    window.alert('Isso é um alerta');
+    alert('Isso é um alerta'); // Funciona
 
-document.querySelector('h1'); // Seleciona o primeiro h1
-document.body; // Retorna o body
+    document.querySelector('h1'); // Seleciona o primeiro h1
+    document.body; // Retorna o body
 
-* window é o objeto global, por isso não precisamos chamar ele na frente dos seus métodos e propriedades.
+    * window é o objeto global, por isso não precisamos chamar ele na frente dos seus métodos e propriedades.
 
-// NODE
+    * NODE
 
-* Toda tag html é representada pelo objeto Element e por isso herda os seus métodos e propriedades. Element é um tipo de objeto Node.
+    * Toda tag html é representada pelo objeto Element e por isso herda os seus métodos e propriedades. Element é um tipo de objeto Node.
 
-const titulo = document.querySelector('h1');
+    const titulo = document.querySelector('h1');
 
-titulo.innerText; // Retorna o texto;
-titulo.classList; // Retorna as classes;
-titulo.id; // Retorna o id;
-titulo.offsetHeight; // Retorna a altura do elemento
+    titulo.innerText; // Retorna o texto;
+    titulo.classList; // Retorna as classes;
+    titulo.id; // Retorna o id;
+    titulo.offsetHeight; // Retorna a altura do elemento
 
-titulo.addEventListener('click', callback);
-// ativa a função callback ao click no titulo
+    titulo.addEventListener('click', callback);
+    / ativa a função callback ao click no titulo
 
 
 
-// EXERCÍCIO
+    * EXERCÍCIO
 
-// Retorne o url da página atual utilizando o objeto window
+    / Retorne o url da página atual utilizando o objeto window
 
-// Seleciona o primeiro elemento da página que possua a classe ativo
+    / Seleciona o primeiro elemento da página que possua a classe ativo
 
-// Retorne a linguagem do navegador
+    / Retorne a linguagem do navegador
 
-// Retorne a largura da janela 
+    / Retorne a largura da janela 
 
+
+
+
+    / Retorne o url da página atual utilizando o objeto window
+    const hrefPagina = window.location.href
+    console.log(hrefPagina)
+
+    / Seleciona o primeiro elemento da página que possua a classe ativo
+    const elementoAtivo = document.querySelector('.ativo')
+
+    / Retorne a linguagem do navegador
+    const linguagem = window.navigator.language
+    console.log(linguagem)
+
+    / Retorne a largura da janela
+    const largura = window.innerWidth
+    console.log(largura)
 
 */
-
-// Retorne o url da página atual utilizando o objeto window
-const hrefPagina = window.location.href
-console.log(hrefPagina)
-
-// Seleciona o primeiro elemento da página que possua a classe ativo
-const elementoAtivo = document.querySelector('.ativo')
-
-// Retorne a linguagem do navegador
-const linguagem = window.navigator.language
-console.log(linguagem)
-
-// Retorne a largura da janela
-const largura = window.innerWidth
-console.log(largura)
